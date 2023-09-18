fedc7eca91415691ba0e124c57ac66e060d770f0
suraj <suraj@funnelstory.io>
2023-09-18 17:20:49 +0530

    update server config

--- index.ts
+++ index.ts
@@ -1,9 +1,49 @@
-const server = Bun.serve({
-  hostname: "::",
-  port: process.env.PORT ?? 3000,
-  fetch(request) {
-    return new Response("Welcome to Bun!");
-  },
-});
+import { Elysia, t } from "elysia";
+import { swagger } from "@elysiajs/swagger";
 
-console.log(`Listening on http://localhost:${server.port}`);
+const app = new Elysia()
+  .use(swagger())
+  .get("/", () => ({ ok: "true" }))
+  .post(
+    "/sign-in",
+    async (context) => {
+      const { set, body } = context;
+
+      if (await signIn(body)) {
+        set.status = 200;
+        return { ok: "true" };
+      } else {
+        set.status = 403;
+        return { ok: "false", error: "invalid username or password" };
+      }
+    },
+    {
+      body: t.Object({
+        username: t.String(),
+        password: t.String(),
+      }),
+    }
+  )
+  .listen({
+    hostname: "::",
+    port: process.env.PORT ?? 3000,
+  });
+
+export type App = typeof app;
+
+function signIn(body: { username: string; password: string }): Promise<any> {
+  return new Promise((resolve) => {
+    if (body.username === "suraj" && body.password === "suraj1294") resolve(1);
+    else resolve(0);
+  });
+}
+
+// const server = Bun.serve({
+//   hostname: "::",
+//   port: process.env.PORT ?? 3000,
+//   fetch(request) {
+//     return new Response("Welcome to Bun!");
+//   },
+// });
+
+// console.log(`Listening on http://localhost:${server.port}`);
